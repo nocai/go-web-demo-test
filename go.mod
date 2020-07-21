3a6538ad324e69270ca2c0c593746d042f483448
Lj.liujun <395822362@qq.com>
2020-07-21 21:33:19 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nocai/go-web-demo-test
+
+go 1.13
+
+require github.com/nocai/go-web-demo/api v0.0.0-20200721131747-4e9cbec4dfca
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nocai/go-web-demo/api"
+)
+
+func main() {
+	_ = api.StringMessage
+	fmt.Println("vim-go")
+}
